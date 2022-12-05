6864a69ee94787c5a90cec4cbfa6da422fc19cc6
Alexssilvadev <112443124+AlexSSilvaDev@users.noreply.github.com>
2022-12-05 19:37:33 -0300

    Add files via upload

---
+++ multiplicadorpy.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um Numero : '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('o dobro de {} vale {} . ' .format(n, d))
+print('o triplo de {} vale {}. \na raiz quadrada de {} é igual a {}.' .format(n, t, n, r))
